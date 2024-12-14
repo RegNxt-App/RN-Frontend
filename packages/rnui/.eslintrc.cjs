cf92ca032a03c849062e9e1f28273846238df76f
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-12-14 19:01:07 +0500

    Remove deprecated UI components from the RNUI package.
    This commit deletes multiple unused UI components, including Accordion, Alert, Avatar, Badge, and others, from the RNUI package. These components are no longer needed and their removal improves codebase cleanliness and reduces maintenance overhead.

---
+++ packages/rnui/.eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: [
+    './node_modules/@hc/config/eslint-preset.cjs',
+    'plugin:storybook/recommended',
+    'plugin:storybook/recommended',
+  ],
+};
