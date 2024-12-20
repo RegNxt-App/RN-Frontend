a36fb6a109d61004c75337347abce860dd02c8a2
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-12-20 10:09:18 +0500

    Added a placeholder for password

--- apps/bird/src/pages/auth/Login.tsx
+++ apps/bird/src/pages/auth/Login.tsx
@@ -85,6 +85,7 @@ export default function Login() {
                 <FormControl>
                   <Input
                     type="password"
+                    placeholder="*******"
                     {...field}
                   />
                 </FormControl>
