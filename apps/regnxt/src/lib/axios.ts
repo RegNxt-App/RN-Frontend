0789d809aef3f5976a5cd535d45e73022291f722
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-12-23 22:55:46 +0500

    Removing console.log cleaning

--- apps/regnxt/src/lib/axios.ts
+++ apps/regnxt/src/lib/axios.ts
@@ -16,8 +16,6 @@ const birdBackendInstance = axios.create({
   baseURL: import.meta.env.VITE_FAST_API_BACKEND,
 });
 
-console.log('orchestra: ', import.meta.env.VITE_ORCHESTRA_API_BACKED);
-
 const orchestraBackendInstance = axios.create({
   baseURL: import.meta.env.VITE_ORCHESTRA_API_BACKED,
 });
