3acf1f38f0477f3dc63e8e4bb8073788a0376d2f
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2025-02-06 06:12:25 +0500

    added sentry

--- apps/regnxt/src/main.tsx
+++ apps/regnxt/src/main.tsx
@@ -6,6 +6,7 @@ import axiosInstance from '@/lib/axios';
 
 import 'flatpickr/dist/flatpickr.min.css';
 
+import * as Sentry from '@sentry/react';
 import {SWRConfig} from 'swr';
 
 import App from './App';
@@ -13,6 +14,15 @@ import ErrorBoundary from './components/ErrorBoundary';
 import {AuthProvider} from './contexts/AuthContext';
 
 import './index.css';
+
+Sentry.init({
+  dsn: 'https://f92b420690bc616e599dd7080de5c12b@o4508769581072384.ingest.de.sentry.io/4508769583956048',
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 const swrConfig = {
   fetcher: (res: string) => axiosInstance.get(res).then((r) => r.data),
